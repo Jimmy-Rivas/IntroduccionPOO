86d02b998e1d9ba04ebd710d04a0c3c61fb38e09
Jimmy Rivas <jimmyrivas568@gmail>
2021-09-21 10:37:17 -0600

    introduccion

---
+++ IntroduccionAPOO/Paciente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionAPOO
+{
+    class Paciente
+    { // Miembros de la clase 
+        //propiedades
+        private string _PacienteId;
+        private int _age;
+        // propiedad autoimplementada {get; set;}
+        public string _addres { get; set; }
+
+        // metodos de propiedad 
+        // obtiene el nombre del paciente
+        public string getPacienteId()
+        {
+            return _PacienteId;
+
+        }
+        public void  setPacienteId(string pacienteid)
+        {
+            _PacienteId = pacienteid;
+        }
+        
+        public int getAge()
+        {
+            return _age;
+        }
+        public void setAge(int age )
+        {
+            _age = age;
+        }
+
+       /* public string getAddress ()
+        {
+            return _addres;
+
+        }
+        
+        public void setAddres(string address)
+        {
+            _addres = address; 
+        } */
+    }
+}
